60b79da6ba0dc30e1662db7cd32dfcc90e611bae
Andrew Whitley <awhitley1233@gmail.com>
2016-09-17 13:46:20 -0400

    set up base files/declared base variables

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var poleOneDisplay = $(''),
+    poleTwoDisplay = $(''),
+    poleThreeDisplay = $('');
+
+var poleOne = [],
+    poleTwo = [],
+    poleThree = [],
+    diskOne = 1,
+    diskTwo = 2,
+    diskThree = 3,
+    diskFour = 4,
+    diskFive = 5;
+
+var towers = {
+  selectSourcePole : function() {
+
+  },
+  moveDisk : function() {
+
+  },
+  checkForWin : function() {
+
+  }
+};
